7a4b30ece5039916edd474ee617eaac280815715
AbuBakr14 <mostafaabubakr9@gmail.com>
2024-08-10 19:09:08 +0300

    create edit note view v1

--- lib/Views/widgets/add_note_bottom_sheet.dart
+++ lib/Views/widgets/add_note_bottom_sheet.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:notes/Views/widgets/custom_button.dart';
 import 'package:notes/Views/widgets/custom_text_field.dart';
 
 class AddNoteBottomSheet extends StatelessWidget {
@@ -8,30 +9,36 @@ class AddNoteBottomSheet extends StatelessWidget {
   Widget build(BuildContext context) {
     return Padding(
       padding: const EdgeInsets.all(16),
-      child: Column(
-        children: [
-          SizedBox(
-            height: 20,
-          ),
-          Text(
-            'Add New Note',
-            style: TextStyle(fontSize: 25, fontWeight: FontWeight.bold),
-          ),
-          SizedBox(
-            height: 25,
-          ),
-          CustomTextField(
-            hint: 'Title',
-            maxlines: 1,
-          ),
-          SizedBox(
-            height: 10,
-          ),
-          CustomTextField(
-            hint: 'Content',
-            maxlines: 5,
-          )
-        ],
+      child: SingleChildScrollView(
+        child: Column(
+          children: [
+            SizedBox(
+              height: 20,
+            ),
+            Text(
+              'Add New Note',
+              style: TextStyle(fontSize: 25, fontWeight: FontWeight.bold),
+            ),
+            SizedBox(
+              height: 25,
+            ),
+            CustomTextField(
+              hint: 'Title',
+              maxlines: 1,
+            ),
+            SizedBox(
+              height: 10,
+            ),
+            CustomTextField(
+              hint: 'Content',
+              maxlines: 5,
+            ),
+            SizedBox(
+              height: 10,
+            ),
+            CustomButton()
+          ],
+        ),
       ),
     );
   }
--- lib/Views/widgets/custom_note_item.dart
+++ lib/Views/widgets/custom_note_item.dart
@@ -1,40 +1,48 @@
 import 'package:flutter/material.dart';
 import 'package:font_awesome_flutter/font_awesome_flutter.dart';
+import 'package:notes/Views/edit_notes_view.dart';
 
 class CustomNoteItem extends StatelessWidget {
   const CustomNoteItem({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return Container(
-      padding: const EdgeInsets.only(bottom: 15, right: 24),
-      decoration: BoxDecoration(
-          color: Colors.blueAccent.withOpacity(0.8),
-          borderRadius: BorderRadius.circular(16)),
-      child: Column(
-        crossAxisAlignment: CrossAxisAlignment.end,
-        children: [
-          ListTile(
-            title: Text(
-              'Flutter Tips',
-              style: TextStyle(fontSize: 26),
+    return GestureDetector(
+      onTap: () {
+        Navigator.push(context, MaterialPageRoute(builder: (context) {
+          return const EditNotesView();
+        }));
+      },
+      child: Container(
+        padding: const EdgeInsets.only(bottom: 15, right: 24),
+        decoration: BoxDecoration(
+            color: Colors.blueAccent.withOpacity(0.8),
+            borderRadius: BorderRadius.circular(16)),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.end,
+          children: [
+            ListTile(
+              title: Text(
+                'Flutter Tips',
+                style: TextStyle(fontSize: 26),
+              ),
+              subtitle: Text('build your career with tharwat samy',
+                  style: TextStyle(fontSize: 20)),
+              trailing: IconButton(
+                  onPressed: () {},
+                  icon: Icon(
+                    FontAwesomeIcons.trash,
+                    // Icons.delete,
+                    color: Colors.black,
+                    size: 30,
+                  )),
             ),
-            subtitle: Text('build your career with tharwat samy',
-                style: TextStyle(fontSize: 20)),
-            trailing: IconButton(
-                onPressed: () {},
-                icon: Icon(
-                  FontAwesomeIcons.trash,
-                  // Icons.delete,
-                  color: Colors.black,
-                  size: 30,
-                )),
-          ),
-          Text(
-            'May21 ,2022',
-            style: TextStyle(color: Colors.black, fontSize: 15),
-          )
-        ],
+            Text(
+              'May21 ,2022',
+              style: TextStyle(color: Colors.black, fontSize: 15),
+            )
+          ],
+        ),
       ),
     );
   }
